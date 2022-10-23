c6227750aef58617ff5896a1501b02bcaee4e755
Brendan Rodrigues <49267534+kylo33bren@users.noreply.github.com>
2022-10-23 10:16:39 +0530

    Added additional functionality
    Added trig functions (sin, cos, tan), added inverse function, e, loge, log, square root, cube root and percentage

--- Calc/Calc/ViewController.swift
+++ Calc/Calc/ViewController.swift
@@ -9,6 +9,7 @@
 // Basic Calculator app in portrait orientation
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     
@@ -91,22 +92,17 @@ class ViewController: UIViewController {
     
     // function for all clear
     @IBAction func AllClear(_ sender: UIButton) {
-        if (ResultLabel.text!.count == 1)
-        {
-            ResultLabel.text = "0"
-            leftside = 0.0
-            rightside = 0.0
-        }
-        else
-        {
-            ResultLabel.text = "0"
-            leftside = 0.0
-            rightside = 0.0
-            
-        }
+        leftside = 0.0
+        rightside = 0.0
+        haveLeftHandSide = false
+        haveRightHandSide = false
+        resultLabelReady = true
+        activeOperator = ""
+        
+        ResultLabel.text = "0"
     }
     
-    //fucntion for performing mathematical operations
+    
     @IBAction func EqualToBtn(_ sender: UIButton){
         
         let button = sender as UIButton
@@ -155,18 +151,36 @@ class ViewController: UIViewController {
         case "-":
             activeOperator = "-"
             ResultLabel.text = "-"
-            print(leftside)
-            print(rightside)
         case "x":
             activeOperator = "x"
             ResultLabel.text = "x"
-            print(leftside)
-            print(rightside)
         case "/":
             activeOperator = "/"
             ResultLabel.text = "/"
-            print(leftside)
-            print(rightside)
+        case "%":
+            activeOperator = "%"
+            ResultLabel.text = "%"
+        case "sqrt":
+            activeOperator = "sqrt"
+            ResultLabel.text = "√"
+        case "cbrt":
+            activeOperator = "cbrt"
+            ResultLabel.text = "∛"
+        case "1/X":
+            activeOperator = "1/X"
+            ResultLabel.text = "1/"
+        case "Sin":
+            activeOperator = "Sin"
+            ResultLabel.text = "Sin"
+        case "Cos":
+            activeOperator = "Cos"
+            ResultLabel.text = "Cos"
+        case "Tan":
+            activeOperator = "Tan"
+            ResultLabel.text = "Tan"
+        case "Log":
+            activeOperator = "Log"
+            ResultLabel.text = "Log"
         case "=":
             if(haveLeftHandSide && haveRightHandSide)
             {
@@ -187,7 +201,7 @@ class ViewController: UIViewController {
         operatorButtons.forEach{(button) in
             
             // added round corners to Button Container
-            buttonContainer.layer.cornerRadius=48
+            buttonContainer.layer.cornerRadius=32
             buttonContainer.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
             // added radius to OperatorButton collection
             button.layer.cornerRadius=5
@@ -200,10 +214,23 @@ class ViewController: UIViewController {
             // added shadow blur to OperatorButton collection
             button.layer.shadowRadius = 3
             
+
+            
         }
 //        OpreatorButton.layer.cornerRadius=5
     }
 
+    
+    @IBAction func e(_ sender: UIButton) {
+        let button = sender as UIButton
+        ResultLabel.text = button.subtitleLabel!.text
+    }
+    
+    @IBAction func Log_e(_ sender: UIButton) {
+        let button = sender as UIButton
+        ResultLabel.text = button.titleLabel!.text
+    }
+    
     
     @IBAction func BtnPlusMinus(_ sender: UIButton) {
      
@@ -236,6 +263,45 @@ class ViewController: UIViewController {
         return leftside / rightside
     }
     
+    func Percent(_ leftside: Float, _ rightside: Float) -> Float
+    {
+        return leftside * 0.01 * rightside
+    }
+    
+    func Root(_ rightside: Float) -> Float
+    {
+        return sqrt(rightside)
+    }
+    
+    func CubeRoot(_ rightside: Float) -> Float
+    {
+        return cbrt(rightside)
+    }
+    
+    func Reciprocal(_ rightside: Float) -> Float
+    {
+        return (1 / rightside)
+    }
+    
+    func Sine(_ rightside: Float) -> Float
+    {
+        return sin((rightside * 3.14159265) / 180)
+    }
+    
+    func Cosine(_ rightside: Float) -> Float
+    {
+        return cos((rightside * 3.14159265) / 180)
+    }
+    
+    func Tangent(_ rightside: Float) -> Float
+    {
+        return tan((rightside * 3.14159265) / 180)
+    }
+
+    func Log10(_ rightside: Float) -> Float
+    {
+        return log10(rightside)
+    }
     
     func Evaluate(){
    
@@ -253,6 +319,30 @@ class ViewController: UIViewController {
         case "/":
             result = Divide(leftside,rightside)
 //            ResultLabel.text =  String(Divide(leftside, rightside))
+        case "%" :
+            result = Percent(leftside,rightside)
+//            ResultLabel.text = String(Percentage(leftside, rightside))
+        case "sqrt":
+            result = Root(rightside)
+//            ResultLabel.text = String(SqRoot(rightside))
+        case "cbrt":
+            result = CubeRoot(rightside)
+//            ResultLabel.text = String(CbRoot(rightside))
+        case "1/X":
+            result = Reciprocal(rightside)
+//            ResultLabel.text = String(Reciprocal(rightside))
+        case "Sin":
+            result = Sine(rightside)
+//            ResultLabel.text = String(Sine(rightside))
+        case "Cos":
+            result = Cosine(rightside)
+//            ResultLabel.text = String(Cosine(rightside))
+        case "Tan":
+            result = Tangent(rightside)
+//            ResultLabel.text = String(Tangent(rightside))
+        case "Log":
+            result = Log10(rightside)
+//             ResultLabel.text = String(Tangent(rightside))
         default:
             result = 0.0
         }
